f8f5c53ec1dcf8d809ec6d655f2be6c141bb6977
Melu <melacoton11@gmail.com>
2023-11-21 10:48:53 -0300

    alt

--- src/Imagen.js
+++ src/Imagen.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Imagen(){
     return(
         <div className="imagen">
-            <img src="2.jpg"/>
+            <img src="2.jpg" alt="imagen decorativa"/>
         </div>
     );
 };
